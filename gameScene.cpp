af1404d03af2c1b8be2ad6f60925c26cbf91c341
Matt Greer <matt.e.greer@gmail.com>
2018-08-12 16:41:32 -0700

    room transition down working

--- gameScene.cpp
+++ gameScene.cpp
@@ -5,35 +5,45 @@ bool isOffscreen(int16_t x, int16_t y) {
 }
 
 void GameScene::detectTileCollisions(void) {
-    uint8_t tile = tileRoom->getTileAt(player.x, player.y);
-    player.onCollide(tile);
+    /* uint8_t tile = tileRoom->getTileAt(player.x, player.y); */
+    /* player.onCollide(tile); */
 }
 
 void GameScene::goToNextRoom(int16_t x, int16_t y) {
     int8_t newRoomIndex = -1;
+    int16_t newVerticalRoomTransitionCount = 0;
+    /* int16_t newHorizontalRoomTransitionCount = 0; */
 
     if (y > HEIGHT) {
         newRoomIndex = map[currentRoomIndex].down;
-        player.moveTo(player.x, 1);
+        newVerticalRoomTransitionCount = HEIGHT;
     } else if (y < 0) {
         newRoomIndex = map[currentRoomIndex].up;
-        player.moveTo(player.x, HEIGHT - 1);
+        /* player.moveTo(player.x, HEIGHT - 1); */
+        newVerticalRoomTransitionCount = -HEIGHT;
     } else if (x < 0) {
         newRoomIndex = map[currentRoomIndex].left;
-        player.moveTo(WIDTH - 1, player.y);
+        /* player.moveTo(WIDTH - 1, player.y); */
     } else if (x > WIDTH) {
         newRoomIndex = map[currentRoomIndex].right;
-        player.moveTo(1, player.y);
+        /* player.moveTo(1, player.y); */
     }
 
     if (newRoomIndex > 0) {
-        currentRoomIndex = newRoomIndex;
-        delete tileRoom;
-        tileRoom = new TileRoom(map[currentRoomIndex].roomDef);
+        nextRoomIndex = newRoomIndex;
+        nextRoom = new TileRoom(map[nextRoomIndex].roomDef);
+
+        if (newVerticalRoomTransitionCount != 0) {
+            verticalRoomTransitionCount = newVerticalRoomTransitionCount;
+        }
     }
 }
 
 Scene GameScene::update(byte frame) {
+    if (verticalRoomTransitionCount > 0 || horizontalRoomTransitionCount > 0) {
+        return GAME;
+    }
+
     if (arduboy->pressed(B_BUTTON)) {
         return TITLE;
     }
@@ -49,7 +59,39 @@ Scene GameScene::update(byte frame) {
     return GAME;
 }
 
-void GameScene::render(byte frame) {
+void GameScene::renderVerticalRoomTransition(uint8_t frame) {
+    renderer->translateY = verticalRoomTransitionCount - HEIGHT;
     tileRoom->render(renderer, frame);
+
+    renderer->translateY = verticalRoomTransitionCount;
+    nextRoom->render(renderer, frame);
+
+    renderer->translateY = verticalRoomTransitionCount - HEIGHT;
     player.render(renderer, frame);
+
+    verticalRoomTransitionCount -= 2;
+
+    if (verticalRoomTransitionCount <= 0) {
+        renderer->translateY = 0;
+        verticalRoomTransitionCount = 0;
+        delete tileRoom;
+        tileRoom = nextRoom;
+        currentRoomIndex = nextRoomIndex;
+        nextRoomIndex = 0;
+        nextRoom = NULL;
+        player.moveTo(player.x, 1);
+    }
+}
+
+void GameScene::renderHorizontalRoomTransition(uint8_t frame) {}
+
+void GameScene::render(uint8_t frame) {
+    if (verticalRoomTransitionCount > 0) {
+        renderVerticalRoomTransition(frame);
+    } else if (horizontalRoomTransitionCount > 0) {
+        renderHorizontalRoomTransition(frame);
+    } else {
+        tileRoom->render(renderer, frame);
+        player.render(renderer, frame);
+    }
 }
--- gameScene.h
+++ gameScene.h
@@ -13,17 +13,27 @@ class GameScene: public BaseScene {
         Player player;
         const MapRoomDef* map;
         uint8_t currentRoomIndex;
+        uint8_t nextRoomIndex;
         TileRoom* tileRoom;
+        TileRoom* nextRoom;
+
+        int16_t horizontalRoomTransitionCount;
+        int16_t verticalRoomTransitionCount;
 
         void detectTileCollisions(void);
         void goToNextRoom(int16_t x, int16_t y);
+        void renderVerticalRoomTransition(uint8_t frame);
+        void renderHorizontalRoomTransition(uint8_t frame);
 
     public:
         GameScene(Arduboy2* arduboy, Renderer* renderer):
             BaseScene(arduboy, renderer),
             player(64, 32),
             map(overworldMap),
-            currentRoomIndex(1)
+            currentRoomIndex(1),
+            nextRoomIndex(0),
+            horizontalRoomTransitionCount(0),
+            verticalRoomTransitionCount(0)
         {
             tileRoom = new TileRoom(map[currentRoomIndex].roomDef);
         }
