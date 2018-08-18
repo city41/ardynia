afbd791950bd6a4d7901ca629c0b16044cba0d76
Matt Greer <matt.e.greer@gmail.com>
2018-08-18 11:09:19 -0700

    inGameMenu: put strings into PROGMEM

--- inGameMenu.cpp
+++ inGameMenu.cpp
@@ -1,6 +1,11 @@
 #include "inGameMenu.h"
 #include "util.h"
 
+const char pauseMsg[] PROGMEM = "pause";
+const char quitMsg[] PROGMEM = "quit";
+
+const char* const menuMessages[] PROGMEM = { pauseMsg, quitMsg };
+
 void InGameMenu::update(Arduboy2* arduboy, byte frame) {
     if (arduboy->justPressed(UP_BUTTON)) {
         decision = clamp(decision - 1, 0, MAX_DECISION);
@@ -12,6 +17,9 @@ void InGameMenu::update(Arduboy2* arduboy, byte frame) {
 }
 
 void InGameMenu::render(Renderer* renderer, byte frame) {
-    renderer->print(10, 10, F("pause"));
-    renderer->print(10, 20, F("quit"));
+    __FlashStringHelper* pauseM = pgm_read_word(menuMessages);
+    __FlashStringHelper* quitM = pgm_read_word(menuMessages + 1);
+
+    renderer->print(10, 10, pauseM);
+    renderer->print(10, 20, quitM);
 }
