86dced36b2db3f277a7e9be3e6d35f364a7234df
Matt Greer <matt.e.greer@gmail.com>
2018-08-18 15:12:44 -0700

    gameScene: use update/render pointers

--- gameScene.cpp
+++ gameScene.cpp
@@ -1,7 +1,18 @@
 #include "gameScene.h"
 
-#define ROOM_TRANSITION_VELOCITY 2
+const uint8_t ROOM_TRANSITION_VELOCITY = 2;
 const uint8_t ROOM_WIDTH = TILES_PER_ROW * TILE_SIZE;
+const uint8_t ROOM_HEIGHT = TILES_PER_COLUMN * TILE_SIZE;
+
+void GameScene::push(UpdatePtr newUpdate, RenderPtr newRender) {
+    nextUpdate = newUpdate;
+    nextRender = newRender;
+}
+
+void GameScene::pop() {
+    nextUpdate = prevUpdate;
+    nextRender = prevRender;
+}
 
 bool isOffscreen(int16_t x, int16_t y) {
     return x < 0 || y < 0 || x > ROOM_WIDTH || y > HEIGHT;
@@ -16,35 +27,28 @@ void GameScene::goToNextRoom(int16_t x, int16_t y) {
     if (x < 0) {
         nextRoomX = tileRoom.x - 1;
         nextRoomY = tileRoom.y;
-        horizontalRoomTransitionCount = -ROOM_WIDTH;
+        roomTransitionCount = ROOM_WIDTH;
 
     } else if (x > ROOM_WIDTH) {
         nextRoomX = tileRoom.x + 1;
         nextRoomY = tileRoom.y;
-        horizontalRoomTransitionCount = ROOM_WIDTH;
-
+        roomTransitionCount = ROOM_WIDTH;
 
     } else if (y < 0) {
         nextRoomX = tileRoom.x;
         nextRoomY = tileRoom.y - 1;
-        verticalRoomTransitionCount = -HEIGHT;
+        roomTransitionCount = ROOM_HEIGHT;
+
     } else if (y > HEIGHT) {
         nextRoomX = tileRoom.x;
         nextRoomY = tileRoom.y + 1;
-        verticalRoomTransitionCount = HEIGHT;
+        roomTransitionCount = ROOM_HEIGHT;
     }
+
+    push(&GameScene::updateRoomTransition, &GameScene::renderRoomTransition);
 }
 
-Scene GameScene::update(byte frame) {
-    if (verticalRoomTransitionCount != 0 || horizontalRoomTransitionCount != 0) {
-        return GAME;
-    }
-
-    if (arduboy->pressed(A_BUTTON) && arduboy->pressed(B_BUTTON)) {
-        menu.update(arduboy, frame);
-        return GAME;
-    }
-
+Scene GameScene::updatePlay(uint8_t frame) {
     player.update(arduboy, frame);
 
     if (isOffscreen(player.x, player.y)) {
@@ -56,109 +60,107 @@ Scene GameScene::update(byte frame) {
     return GAME;
 }
 
-void GameScene::renderVerticalRoomTransition(uint8_t frame) {
-    bool goingToRoomBelow = verticalRoomTransitionCount > 0;
-
-    if (goingToRoomBelow) {
-        renderer->translateY = verticalRoomTransitionCount - HEIGHT;
-    } else {
-        renderer->translateY = HEIGHT + verticalRoomTransitionCount;
-    }
+void GameScene::renderPlay(uint8_t frame) {
     tileRoom.render(renderer, frame);
-
-    if (goingToRoomBelow) {
-        renderer->translateY = verticalRoomTransitionCount;
-    } else {
-        renderer->translateY = verticalRoomTransitionCount;
-    }
-
-    tileRoom.render(renderer, frame, nextRoomX, nextRoomY);
-
-    if (goingToRoomBelow) {
-        renderer->translateY = verticalRoomTransitionCount - HEIGHT;
-    } else {
-        renderer->translateY = HEIGHT + verticalRoomTransitionCount;
-    }
-
     player.render(renderer, frame);
-
-    if (goingToRoomBelow) {
-        verticalRoomTransitionCount -= ROOM_TRANSITION_VELOCITY;
-    } else {
-        verticalRoomTransitionCount += ROOM_TRANSITION_VELOCITY;
-    }
-
-    if (verticalRoomTransitionCount == 0) {
-        renderer->translateY = 0;
-        tileRoom.x = nextRoomX;
-        tileRoom.y = nextRoomY;
-
-        if (goingToRoomBelow) {
-            player.moveTo(player.x, 1);
-        } else {
-            player.moveTo(player.x, HEIGHT - 1);
-        }
-    }
 }
 
-void GameScene::renderHorizontalRoomTransition(uint8_t frame) {
-    bool goingToRoomToRight = horizontalRoomTransitionCount > 0;
+Scene GameScene::updateMenu(uint8_t frame) {
+    menu.update(arduboy, frame);
+    return GAME;
+}
 
-    if (goingToRoomToRight) {
-        renderer->translateX = horizontalRoomTransitionCount - ROOM_WIDTH;
-    } else {
-        renderer->translateX = ROOM_WIDTH + horizontalRoomTransitionCount;
-    }
-    tileRoom.render(renderer, frame);
+void GameScene::renderMenu(uint8_t frame) {
+    menu.render(renderer, frame);
+}
 
-    if (goingToRoomToRight) {
-        renderer->translateX = horizontalRoomTransitionCount;
-    } else {
-        renderer->translateX = horizontalRoomTransitionCount;
-    }
+Scene GameScene::updateRoomTransition(uint8_t frame) {
+    roomTransitionCount -= ROOM_TRANSITION_VELOCITY;
 
-    tileRoom.render(renderer, frame, nextRoomX, nextRoomY);
+    if (roomTransitionCount == 0) {
 
-    if (goingToRoomToRight) {
-        renderer->translateX = horizontalRoomTransitionCount - ROOM_WIDTH;
-    } else {
-        renderer->translateX = ROOM_WIDTH + horizontalRoomTransitionCount;
-    }
+        if (nextRoomX < tileRoom.x) {
+            player.moveTo(ROOM_WIDTH - 1, player.y);
+        } else if (nextRoomX > tileRoom.x) {
+            player.moveTo(1, player.y);
+        } else if (nextRoomY < tileRoom.y) {
+            player.moveTo(player.x, ROOM_HEIGHT - 1);
+        } else {
+            player.moveTo(player.x, 1);
+        }
 
-    player.render(renderer, frame);
-
-    if (goingToRoomToRight) {
-        horizontalRoomTransitionCount -= ROOM_TRANSITION_VELOCITY;
-    } else {
-        horizontalRoomTransitionCount += ROOM_TRANSITION_VELOCITY;
-    }
-
-    if (horizontalRoomTransitionCount == 0) {
-        renderer->translateX = 0;
         tileRoom.x = nextRoomX;
         tileRoom.y = nextRoomY;
 
-        if (goingToRoomToRight) {
-            player.moveTo(1, player.y);
-        } else {
-            player.moveTo(ROOM_WIDTH - 1, player.y);
+        pop();
+    }
+
+    return GAME;
+}
+
+void GameScene::renderRoomTransition(uint8_t frame) {
+    int16_t translateX = 0, translateY = 0;
+
+    // draw current room translated
+    if (nextRoomX < tileRoom.x) {
+        translateX = ROOM_WIDTH - roomTransitionCount;
+    } else if (nextRoomX > tileRoom.x) {
+        translateX = roomTransitionCount - ROOM_WIDTH;
+    } else if (nextRoomY < tileRoom.y) {
+        translateY = ROOM_HEIGHT - roomTransitionCount;
+    } else if (nextRoomY > tileRoom.y) {
+        translateY = roomTransitionCount - ROOM_HEIGHT;
+    }
+
+    renderer->translateX = translateX;
+    renderer->translateY = translateY;
+    tileRoom.render(renderer, frame);
+
+    // draw next room translated
+    int16_t s = (translateX < 0 || translateY < 0) ? 1 : -1;
+
+    renderer->translateX = translateX != 0 ? translateX + (ROOM_WIDTH * s) : 0;
+    renderer->translateY = translateY != 0 ? translateY + (ROOM_HEIGHT * s) : 0;
+    tileRoom.render(renderer, frame, nextRoomX, nextRoomY);
+    
+    // draw player translated
+    renderer->translateX = translateX;
+    renderer->translateY = translateY;
+    player.render(renderer, frame);
+
+    renderer->translateX = 0;
+    renderer->translateY = 0;
+}
+
+Scene GameScene::update(uint8_t frame) {
+    if (arduboy->pressed(A_BUTTON) && arduboy->pressed(B_BUTTON)) {
+        if (currentUpdate != &GameScene::updateMenu) {
+            push(&GameScene::updateMenu, &GameScene::renderMenu);
+        }
+    } else {
+        if (currentUpdate == &GameScene::updateMenu) {
+            // TODO: respond to menu choice here
+            pop();
         }
     }
+
+    Scene nextScene = (this->*currentUpdate)(frame);
+
+    if (nextUpdate != NULL) {
+        prevUpdate = currentUpdate;
+        currentUpdate = nextUpdate;
+        nextUpdate = NULL;
+    }
+
+    return nextScene;
 }
 
 void GameScene::render(uint8_t frame) {
-    if (verticalRoomTransitionCount != 0) {
-        renderVerticalRoomTransition(frame);
-    } else if (horizontalRoomTransitionCount != 0) {
-        renderHorizontalRoomTransition(frame);
-    } else {
+    (this->*currentRender)(frame);
 
-        if (arduboy->pressed(A_BUTTON) && arduboy->pressed(B_BUTTON)) {
-            menu.render(renderer, frame);
-            return;
-        }
-
-        tileRoom.render(renderer, frame);
-        player.render(renderer, frame);
+    if (nextRender != NULL) {
+        prevRender = currentRender;
+        currentRender = nextRender;
+        nextRender = NULL;
     }
 }
--- gameScene.h
+++ gameScene.h
@@ -10,6 +10,9 @@
 #include "inGameMenu.h"
 
 class GameScene: public BaseScene {
+    typedef Scene (GameScene::*UpdatePtr)(uint8_t);
+    typedef void (GameScene::*RenderPtr)(uint8_t);
+
     private:
         Player player;
         const uint8_t* map;
@@ -19,13 +22,32 @@ class GameScene: public BaseScene {
         TileRoom tileRoom;
         InGameMenu menu;
 
-        int16_t horizontalRoomTransitionCount;
-        int16_t verticalRoomTransitionCount;
+        int16_t roomTransitionCount;
+
+        UpdatePtr prevUpdate;
+        UpdatePtr currentUpdate;
+        UpdatePtr nextUpdate;
+
+        RenderPtr prevRender;
+        RenderPtr currentRender;
+        RenderPtr nextRender;
 
         void detectTileCollisions(void);
         void goToNextRoom(int16_t x, int16_t y);
         void renderVerticalRoomTransition(uint8_t frame);
         void renderHorizontalRoomTransition(uint8_t frame);
+
+        Scene updatePlay(uint8_t frame);
+        void renderPlay(uint8_t frame);
+
+        Scene updateRoomTransition(uint8_t frame);
+        void renderRoomTransition(uint8_t frame);
+
+        Scene updateMenu(uint8_t frame);
+        void renderMenu(uint8_t frame);
+
+        void push(UpdatePtr newUpdate, RenderPtr newRender);
+        void pop();
 
     public:
         GameScene(Arduboy2* arduboy, Renderer* renderer):
@@ -37,13 +59,18 @@ class GameScene: public BaseScene {
             nextRoomY(0),
             tileRoom(map, tiles, 0, 0),
             menu(),
-            horizontalRoomTransitionCount(0),
-            verticalRoomTransitionCount(0)
+            roomTransitionCount(0),
+            prevUpdate(NULL),
+            currentUpdate(&GameScene::updatePlay),
+            nextUpdate(NULL),
+            prevRender(NULL),
+            currentRender(&GameScene::renderPlay),
+            nextRender(NULL)
         {
         }
 
-        Scene update(byte frame);
-        void render(byte frame);
+        Scene update(uint8_t frame);
+        void render(uint8_t frame);
 };
 
 #endif
