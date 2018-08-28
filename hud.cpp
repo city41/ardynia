2f7b6f5b4b5f3aceb08f7125c40a76a3c2523285
Matt Greer <matt.e.greer@gmail.com>
2018-08-27 19:14:05 -0700

    heartDots in hud

--- hud.cpp
+++ hud.cpp
@@ -1,15 +1,11 @@
 #include "hud.h"
 #include "bitmaps.h"
+
 void drawDots(Renderer* renderer, uint8_t x, uint8_t y, uint8_t count, uint8_t maxCount, uint8_t frame) {
     uint8_t origX = x;
 
     for (uint8_t i = 0; i < maxCount; ++i) {
-        if (i < count) {
-            renderer->fillRect(x, y, 3, 3, WHITE);
-        } else {
-            renderer->drawRect(x, y, 3, 3, WHITE);
-        }
-
+        renderer->drawOverwrite(x, y, heartDot_tiles, i < count ? 0 : 1);
         x += 4;
 
         if (i == 3) {
@@ -27,7 +23,7 @@ void Hud::render(Renderer* renderer, uint8_t frame, Player& player, uint8_t room
 
     // secondary item
     if (player.bButtonEntityType != UNSET) {
-        renderer->drawOverwrite(0, 12, itemIcons_tiles, player.bButtonEntityType - 4);
+        renderer->drawOverwrite(4, 12, itemIcons_tiles, player.bButtonEntityType - 4);
     }
 
     renderer->drawSelfMasked(1, 12, hudBFrame_tiles, 0);
