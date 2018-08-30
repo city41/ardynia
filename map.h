8f28d58f47b11f97d0d84b88c2d5b13523c23686
Matt Greer <matt.e.greer@gmail.com>
2018-08-30 15:15:26 -0700

    tiled: dungeon overlay png

--- map.h
+++ map.h
@@ -4,10 +4,10 @@
 #include <Arduino.h>
 #include "renderer.h"
 
-const uint8_t MAX_NUM_VISITED_ROOMS = 12 * 8;
+const uint8_t MAX_NUM_VISITED_ROOMS = 12 * 7;
 
 struct Map {
-    static uint8_t visitedRooms[MAX_NUM_VISITED_ROOMS / 8];
+    static uint8_t visitedRooms[MAX_NUM_VISITED_ROOMS / 8 + 1];
 
     static void render(Renderer* renderer, uint8_t mapWidthInRooms, uint8_t currentRoomX, uint8_t currentRoomY);
     static void visitRoom(uint8_t roomX, uint8_t roomY, uint8_t mapWidthInRooms);
---
+++ maptool/buildTiledDungeonColorOverlay.js
@@ -0,0 +1,57 @@
+// generates an image to drop into tiled so that the dungeons are color coded
+
+const Canvas = require("canvas");
+const fs = require("fs");
+
+const TILE_SIZE = 16;
+const WIDTH_IN_ROOMS = 12;
+const HEIGHT_IN_ROOMS = 7;
+const TILES_PER_ROW = 7;
+const TILES_PER_COLUMN = 4;
+const ROOM_WIDTH_PX = TILES_PER_ROW * TILE_SIZE;
+const ROOM_HEIGHT_PX = TILES_PER_COLUMN * TILE_SIZE;
+const IMAGE_WIDTH_PX = WIDTH_IN_ROOMS * ROOM_WIDTH_PX;
+const IMAGE_HEIGHT_PX = HEIGHT_IN_ROOMS * ROOM_HEIGHT_PX;
+
+const DATA = [
+    [2, 2, 2, 3, 0, 3, 3, 3, 3, 5, 5, 5],
+    [1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3],
+    [1, 1, 1, 1, 0, 2, 2, 2, 3, 2, 3, 3],
+    [4, 1, 1, 1, 1, 1, 2, 2, 3, 2, 3, 3],
+    [4, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+    [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3]
+];
+
+const COLORS = [
+    "rgba(0,0,0,0.3)",
+    "rgba(255,0,0,0.3)",
+    "rgba(255,255,0,0.3)",
+    "rgba(0,255,0,0.3)",
+    "rgba(0,0,255,0.3)",
+    "rgba(0,255,255,0.3)"
+];
+
+const canvas = new Canvas(IMAGE_WIDTH_PX, IMAGE_HEIGHT_PX);
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgba(0,0,0,0)";
+ctx.fillRect(0, 0, IMAGE_WIDTH_PX, IMAGE_HEIGHT_PX);
+
+for (let y = 0; y < DATA.length; ++y) {
+    for (let x = 0; x < DATA[y].length; ++x) {
+        const colorIndex = DATA[y][x];
+        const color = COLORS[colorIndex];
+        console.log("color", color);
+        ctx.fillStyle = color;
+        ctx.fillRect(
+            x * ROOM_WIDTH_PX,
+            y * ROOM_HEIGHT_PX,
+            ROOM_WIDTH_PX,
+            ROOM_HEIGHT_PX
+        );
+    }
+}
+
+const buf = canvas.toBuffer();
+fs.writeFileSync("dungeonOverlay.png", buf);
