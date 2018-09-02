dc7a0054ce5e4b154c6a700252ab75ee57534028
Matt Greer <matt.e.greer@gmail.com>
2018-09-02 08:47:56 -0700

    death: item drop is just random

--- death.cpp
+++ death.cpp
@@ -2,28 +2,17 @@
 #include "state.h"
 #include "player.h"
 
+/**
+ * an entity that shows a death "splat" then rolls dice to
+ * see if the player gets an item drop. originally the item drop considered
+ * the player's health and bomb levels, but that cost way more bytes than the value
+ * it provided
+ */
 EntityType Death::update(Entity* me, BaseEntity* player, Arduboy2* arduboy, uint8_t frame) {
     if (me->duration > 0) {
         me->duration -= 1;
     } else {
         me->type = UNSET;
-
-        // let's see if the player randomly earns an item
-        uint8_t numBombs = ((Player*)player)->numBombs;
-        uint8_t playerHealth = ((Player*)player)->health;
-        bool playerIsFullHealth = State::gameState.totalHealth == playerHealth;
-        // if the user hasn't even acquired bombs yet, then in this context they are "full" on bombs
-        bool playerIsFullBombs = numBombs == MAX_BOMB_COUNT || State::gameState.numAcquiredItems < 3;
-
-        if (
-            playerIsFullHealth
-            && playerIsFullBombs) {
-
-            // player has full health and bombs, we have nothing to offer them.
-            // we do this because we don't want a heart/bomb to take up a precious
-            // entity slot that could otherwise be used for projectiles and the like
-            return UNSET;
-        }
 
         bool willAward = random(0, 3) == 1;
 
@@ -31,18 +20,6 @@ EntityType Death::update(Entity* me, BaseEntity* player, Arduboy2* arduboy, uint
             return UNSET;
         }
 
-        // player doesn't need bombs, so heart it is
-        if (playerIsFullBombs) {
-            return HEART;
-        }
-
-
-        // player doesn't need health, so bomb it is
-        if (playerIsFullHealth) {
-            return BOMB;
-        }
-        
-        // player needs both bomb and heart, so coin flip to pick
         if (random(0, 2) == 0) {
             return BOMB;
         } else {
