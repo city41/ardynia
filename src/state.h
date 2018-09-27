ec421e03692dceca44abf11c802ef89d3636b86b
Matt Greer <matt.e.greer@gmail.com>
2018-09-26 18:03:50 -0700

    State: add one bye to roomStates

--- src/state.h
+++ src/state.h
@@ -7,7 +7,7 @@
 // as a single "dungeon"
 const uint8_t MAX_DUNGEON_COUNT = 5;
 const uint8_t MAX_ROOM_COUNT = 8 * 8 + 7 * 7;
-const uint8_t DUNGEON_ROOM_OFFSET = 49;
+const uint8_t DUNGEON_ROOM_OFFSET = 7 * 7;
 const uint8_t MAX_KEYS = 6;
 
 const uint8_t GAME_ID = 81;
@@ -25,7 +25,10 @@ struct GameState {
     // can only have ONE thing about it that is stateful. That means each
     // room can only have one chest OR one locked door, etc. It is not allowed
     // for a single room to have both a chest and a locked door for example, one or the other
-    uint8_t roomStates[MAX_ROOM_COUNT / 8];
+    // 
+    // The +1 is because the number of rooms is not necessarily divisible by 8, there
+    // are some leftover bits we need to store
+    uint8_t roomStates[MAX_ROOM_COUNT / 8 + 1];
 };
 
 class State {
