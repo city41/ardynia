edfb231e74b3924310cc578d91c530fc2a837b73
Matt Greer <matt.e.greer@gmail.com>
2018-09-26 18:25:57 -0700

    TriggerDoor: same functionality, fewer bytes

--- src/entities/triggerDoor.cpp
+++ src/entities/triggerDoor.cpp
@@ -2,23 +2,19 @@
 #include "../tileBitmaps.h"
 
 EntityType TriggerDoor::update(Entity* me, Entity& player, Game& game, Arduboy2Base& arduboy, uint8_t frame) {
-    if (me->duration < 20) {
-        me->health = 1;
+    // if the door just spawned on top of the player, wait
+    // for the player to move out of the way before coming on line
+    if (me->health == 0 && !player.overlaps(*me)) {
         me->tiles = triggerDoor_tiles;
-    } else {
-        me->duration -= 1;
+        me->health = 1;
     }
 
     return UNSET;
 }
 
 EntityType TriggerDoor::onCollide(Entity* me, Entity& other, Entity& player, Game& game) {
-    if (other.type == PLAYER) {
-        if (me->health == 0) {
-            me->duration += 1;
-        } else {
-            other.undoMove();
-        }
+    if (other.type == PLAYER && me->health) {
+        other.undoMove();
     }
 
     return UNSET;
