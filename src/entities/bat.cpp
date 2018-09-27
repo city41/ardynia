fd2a9797ff4cb931d80986a10ecfaee796d3a85e
Matt Greer <matt.e.greer@gmail.com>
2018-09-26 19:10:13 -0700

    bat: be dormant far less often

--- src/entities/bat.cpp
+++ src/entities/bat.cpp
@@ -2,21 +2,9 @@
 #include "../util.h"
 #include "../game.h"
 
-EntityType Bat::onCollide(Entity* me, Entity& other, Entity& player, Game& game) {
-    if (other.damage > 0 || other.type == BOOMERANG) {
-        if (other.type == BOOMERANG) {
-            other.duration = 0;
-        }
-        me->type = UNSET;
-        return ITEM_DROP;
-    }
-
-    return UNSET;
-}
-
 EntityType Bat::update(Entity* me, Entity& player, Game& game, Arduboy2Base& arduboy, uint8_t frame) {
     if (me->duration == 0 || Util::isOffScreen(me->x, me->y, 8)) {
-        if (frame < 24) {
+        if (frame < 24 && (me->prevX || me->prevY)) {
             me->prevX = me->prevY = 0;
         } else {
             me->prevX = random(-1, 2);
@@ -34,6 +22,18 @@ EntityType Bat::update(Entity* me, Entity& player, Game& game, Arduboy2Base& ard
         if (me->prevX || me->prevY) {
             me->currentFrame = 1 - me->currentFrame;
         }
+    }
+
+    return UNSET;
+}
+
+EntityType Bat::onCollide(Entity* me, Entity& other, Entity& player, Game& game) {
+    if (other.damage > 0 || other.type == BOOMERANG) {
+        if (other.type == BOOMERANG) {
+            other.duration = 0;
+        }
+        me->type = UNSET;
+        return ITEM_DROP;
     }
 
     return UNSET;
