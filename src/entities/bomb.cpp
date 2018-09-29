e43908b0165ae252b8c18e817e97f846fbcf74b7
Matt Greer <matt.e.greer@gmail.com>
2018-09-29 10:28:22 -0700

    bomb: spawn code not needed

--- src/entities/bomb.cpp
+++ src/entities/bomb.cpp
@@ -1,12 +1,6 @@
 #include "bomb.h"
 
 EntityType Bomb::update(Entity* me, Entity& player, Game& game, Arduboy2Base& arduboy, uint8_t frame) {
-    // initial duration? then "spawn"
-    if (me->duration == 150) {
-        me->tookDamageCount = 0;
-        me->mirror = 0;
-    }
-
     // health is being used for "is dormant"
     // health=0 -> dormant item drop
     // health=1 -> active bomb that will explode
