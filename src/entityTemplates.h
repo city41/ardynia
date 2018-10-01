a028a80073edce0cbdf96f7ed262e01bf392e922
Matt Greer <matt.e.greer@gmail.com>
2018-09-30 17:53:49 -0700

    dungeon 2 good

--- src/entityTemplates.h
+++ src/entityTemplates.h
@@ -113,7 +113,7 @@ const uint8_t PROGMEM entityProperties[NUM_ENTITIES * NUM_ENTITY_PROPS] = {
 
     // 11, BLOB_MOTHER
     widthAndHeight(15, 16),
-    healthAndDamage(15, 1),
+    healthAndDamage(6, 1),
     10,
     true,
 
