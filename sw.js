4d8507c45c320371ea6c0a8c06cbbd10cef552f4
Matt Greer <matt.e.egreer@gmail.com>
2018-12-02 19:20:31 -0800

    website: remove it, point to city41.games

--- sw.js
+++ sw.js
@@ -23,56 +23,7 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [
-  {
-    "url": "webpack-runtime-5ee2c039ecc41a3dc0ab.js"
-  },
-  {
-    "url": "app-13bf44fd66723268e95d.js"
-  },
-  {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-8e1f2b96809d702bd6a2.js"
-  },
-  {
-    "url": "index.html",
-    "revision": "16386a6c71b3f7c23b9ae3ce30a30c7c"
-  },
-  {
-    "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2991ebc866dfe1b987bc47a41c80ffe1"
-  },
-  {
-    "url": "component---src-pages-index-js.7a2265121c7c0d8a7eae.css"
-  },
-  {
-    "url": "component---src-pages-index-js-ae7463d0b0aac3745330.js"
-  },
-  {
-    "url": "0-89b37cff2f01712aedf3.js"
-  },
-  {
-    "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
-    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
-  },
-  {
-    "url": "component---src-pages-404-js.a1744392d529d6baf4c4.css"
-  },
-  {
-    "url": "component---src-pages-404-js-1246a1fd63e2b8be86b8.js"
-  },
-  {
-    "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
-    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
-  },
-  {
-    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
-    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
-  },
-  {
-    "url": "manifest.webmanifest",
-    "revision": "9f7ac9d1e96bc807b32f5e8de83e6451"
-  }
-].concat(self.__precacheManifest || []);
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
